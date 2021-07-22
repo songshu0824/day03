5551b6f7f0af7e4259c83f41e2c407a6661668b6
songshu <yxc852814721@163.com>
2021-07-22 22:09:32 +0800

    second commit

--- src/main/java/com/songshu/day03/Day03Application.java
+++ src/main/java/com/songshu/day03/Day03Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Day03Application {
 
     public static void main(String[] args) {
+        System.out.println("hello git");
         SpringApplication.run(Day03Application.class, args);
     }
 
