c63bd2ccc3d7ca4a272cb34e396c45be6cbb2cae
songshu <yxc852814721@163.com>
2021-07-22 22:10:51 +0800

    second commit

--- src/main/java/com/songshu/day03/Day03Application.java
+++ src/main/java/com/songshu/day03/Day03Application.java
@@ -8,6 +8,7 @@ public class Day03Application {
 
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("蓝色的直接提交");
         SpringApplication.run(Day03Application.class, args);
     }
 
