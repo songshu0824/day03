6d58ea771107514a6b4959b480c67d0692013055
songshu <yxc852814721@163.com>
2021-07-22 22:22:20 +0800

    four commit

--- src/main/java/com/songshu/day03/Day03Application.java
+++ src/main/java/com/songshu/day03/Day03Application.java
@@ -9,6 +9,7 @@ public class Day03Application {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("蓝色的直接提交");
+        System.out.println("hot-fix");
         SpringApplication.run(Day03Application.class, args);
     }
 
